be0a3260eca842de29c14bd9508d082def4ba476
Serg Klukvin <klukvin@ukr.net>
2020-06-25 23:20:28 +0300

    end

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 function getData() {
-  console.log(search.value);
-
   let req = new XMLHttpRequest();
 
   if (window.XMLHttpRequest) {
     req = new XMLHttpRequest();
   } else {
-    req = new ActiveXObject("MicrosoftXMLHTTP");
+    req = new ActiveXObject('MicrosoftXMLHTTP');
   }
 
   req.onreadystatechange = function () {
@@ -17,51 +15,53 @@ function getData() {
     }
   };
 
-  req.open("GET", `https://api.github.com/users/${search.value}`, true);
+  req.open('GET', `https://api.github.com/users/${search.value}`, true);
 
   req.send();
 
-  search.value = "";
-  let delActive = document.getElementsByTagName("label");
-  delActive[0].className = "";
+  search.value = '';
+  let delActive = document.getElementsByTagName('label');
+  delActive[0].className = '';
 }
 
 function reqFun(data) {
-  document.getElementById("result").innerHTML = "";
-  console.log(data);
+  document.getElementById('result').innerHTML = '';
 
   let userData = JSON.parse(data);
-  console.log(userData.avatar_url);
 
-  let createDivColumnOne = document.createElement("div");
-  createDivColumnOne.setAttribute("class", "columnOne");
+  let createDivColumnOne = document.createElement('div');
+  createDivColumnOne.setAttribute('class', 'columnOne');
 
-  let createDivRowOne = document.createElement("div");
-  createDivRowOne.setAttribute("class", "rowText");
+  let createDivRowOne = document.createElement('div');
+  createDivRowOne.setAttribute('class', 'rowText');
 
-  let createDivRowTwo = document.createElement("div");
-  createDivRowTwo.setAttribute("class", "rowText");
+  let createDivRowTwo = document.createElement('div');
+  createDivRowTwo.setAttribute('class', 'rowText');
 
-  let createDivRowThree = document.createElement("div");
-  createDivRowThree.setAttribute("class", "rowText");
+  let createDivRowThree = document.createElement('div');
+  createDivRowThree.setAttribute('class', 'rowText');
 
-  let createImg = document.createElement("img");
-  createImg.setAttribute("src", `${userData.avatar_url}`);
+  let createImg = document.createElement('img');
+  createImg.setAttribute('src', `${userData.avatar_url}`);
 
-  let createP_one = document.createElement("p");
-  createP_one.setAttribute("class", "title");
-  createP_one.innerText = "Name";
+  let createP_one = document.createElement('p');
+  createP_one.setAttribute('class', 'title');
+  createP_one.innerText = 'Name';
 
-  let createP_one_one = document.createElement("p");
-  createP_one_one.setAttribute("class", "resultText");
-  createP_one_one.innerText = `${userData.name}`;
+  let createP_one_one = document.createElement('p');
+  createP_one_one.setAttribute('class', 'resultText');
+  if (userData.name === null) {
+    createP_one_one.innerText = `No name`;
+  } else {
+    createP_one_one.innerText = `${userData.name}`;
+  }
 
-  let createP_two = document.createElement("p");
-  createP_two.setAttribute("class", "title");
-  createP_two.innerText = "Login";
+  let createP_two = document.createElement('p');
+  createP_two.setAttribute('class', 'title');
+  createP_two.innerText = 'Login';
 
-  let createP_two_two = document.createElement("p");
-  createP_two_two.setAttribute("class", "resultText");
+  let createP_two_two = document.createElement('p');
+  createP_two_two.setAttribute('class', 'resultText');
   createP_two_two.innerText = `${userData.login}`;
 
   createDivColumnOne.appendChild(createDivRowOne);
@@ -78,84 +78,95 @@ function reqFun(data) {
 
   result.appendChild(createDivColumnOne);
 
-  let createDivColumnTwo = document.createElement("div");
-  createDivColumnTwo.setAttribute("class", "columnTwo");
+  let createDivColumnTwo = document.createElement('div');
+  createDivColumnTwo.setAttribute('class', 'columnTwo');
 
-  let createDivRowFour = document.createElement("div");
-  createDivRowFour.setAttribute("class", "rowText");
+  let createDivRowFour = document.createElement('div');
+  createDivRowFour.setAttribute('class', 'rowText');
 
-  let createDivRowFive = document.createElement("div");
-  createDivRowFive.setAttribute("class", "rowText");
+  let createDivRowFive = document.createElement('div');
+  createDivRowFive.setAttribute('class', 'rowText');
 
-  let createDivRowSix = document.createElement("div");
-  createDivRowSix.setAttribute("class", "rowText");
+  let createDivRowSix = document.createElement('div');
+  createDivRowSix.setAttribute('class', 'rowText');
 
-  let createDivRowSeven = document.createElement("div");
-  createDivRowSeven.setAttribute("class", "rowText");
+  let createDivRowSeven = document.createElement('div');
+  createDivRowSeven.setAttribute('class', 'rowText');
 
-  let createDivRowEight = document.createElement("div");
-  createDivRowEight.setAttribute("class", "rowText");
+  let createDivRowEight = document.createElement('div');
+  createDivRowEight.setAttribute('class', 'rowText');
 
-  let createP_three = document.createElement("p");
-  createP_three.setAttribute("class", "title");
-  createP_three.innerText = "Url to GitHub:";
+  let createP_three = document.createElement('p');
+  createP_three.setAttribute('class', 'title');
+  createP_three.innerText = 'Url to GitHub:';
 
-  let createP_three_three = document.createElement("p");
-  createP_three_three.setAttribute("class", "resultText");
+  let createP_three_three = document.createElement('p');
+  createP_three_three.setAttribute('class', 'resultText');
 
-  let createP_three_three_a = document.createElement("a");
-  createP_three_three_a.setAttribute("href", `${userData.url}`);
-  createP_three_three_a.setAttribute("target", "_blank");
+  let createP_three_three_a = document.createElement('a');
+  createP_three_three_a.setAttribute('href', `${userData.url}`);
+  createP_three_three_a.setAttribute('target', '_blank');
   createP_three_three_a.innerText = `${userData.url}`;
 
-  let createP_four = document.createElement("p");
-  createP_four.setAttribute("class", "title");
-  createP_four.innerText = "Blog:";
+  let createP_four = document.createElement('p');
+  createP_four.setAttribute('class', 'title');
+  createP_four.innerText = 'Blog:';
 
-  let createP_four_four = document.createElement("p");
-  createP_four_four.setAttribute("class", "resultText");
+  let createP_four_four = document.createElement('p');
+  createP_four_four.setAttribute('class', 'resultText');
 
-  let createP_four_four_a = document.createElement("a");
-  createP_four_four_a.setAttribute("href", `${userData.url}`);
-  createP_four_four_a.setAttribute("target", "_blank");
-  createP_four_four_a.innerText = `${userData.blog}`;
+  let createP_four_four_a = document.createElement('a');
+  createP_four_four_a.setAttribute('target', '_blank');
+  if (userData.blog.length === 0) {
+    createP_four_four.innerText = `No blog`;
+  } else {
+    createP_four_four_a.innerText = `${userData.blog}`;
+  }
 
-  let createP_five = document.createElement("p");
-  createP_five.setAttribute("class", "title");
-  createP_five.innerText = "City:";
+  let createP_five = document.createElement('p');
+  createP_five.setAttribute('class', 'title');
+  createP_five.innerText = 'City:';
 
-  let createP_five_five = document.createElement("p");
-  createP_five_five.setAttribute("class", "resultText");
-  createP_five_five.innerText = `${userData.location}`;
+  let createP_five_five = document.createElement('p');
+  createP_five_five.setAttribute('class', 'resultText');
+  if (userData.location === null) {
+    createP_five_five.innerText = `No city`;
+  } else {
+    createP_five_five.innerText = `${userData.location}`;
+  }
 
-  let createP_six = document.createElement("p");
-  createP_six.setAttribute("class", "title");
-  createP_six.innerText = "Email:";
+  let createP_six = document.createElement('p');
+  createP_six.setAttribute('class', 'title');
+  createP_six.innerText = 'Email:';
 
-  let createP_six_six = document.createElement("p");
-  createP_six_six.setAttribute("class", "resultText");
-  createP_six_six.innerText = `${userData.email}`;
+  let createP_six_six = document.createElement('p');
+  createP_six_six.setAttribute('class', 'resultText');
+  if (userData.email === null) {
+    createP_six_six.innerText = `No email`;
+  } else {
+    createP_six_six.innerText = `${userData.email}`;
+  }
 
-  let createDivFlOne = document.createElement("div");
-  createDivFlOne.setAttribute("class", "fl");
+  let createDivFlOne = document.createElement('div');
+  createDivFlOne.setAttribute('class', 'fl');
 
-  let createDivFlTwo = document.createElement("div");
-  createDivFlTwo.setAttribute("class", "fl");
+  let createDivFlTwo = document.createElement('div');
+  createDivFlTwo.setAttribute('class', 'fl');
 
-  let createP_seven = document.createElement("p");
-  createP_seven.setAttribute("class", "title");
-  createP_seven.innerText = "Followers:";
+  let createP_seven = document.createElement('p');
+  createP_seven.setAttribute('class', 'title');
+  createP_seven.innerText = 'Followers:';
 
-  let createP_seven_seven = document.createElement("p");
-  createP_seven_seven.setAttribute("class", "resultText");
+  let createP_seven_seven = document.createElement('p');
+  createP_seven_seven.setAttribute('class', 'resultText');
   createP_seven_seven.innerText = `${userData.followers}`;
 
-  let createP_eight = document.createElement("p");
-  createP_eight.setAttribute("class", "title");
-  createP_eight.innerText = "Following:";
+  let createP_eight = document.createElement('p');
+  createP_eight.setAttribute('class', 'title');
+  createP_eight.innerText = 'Following:';
 
-  let createP_eight_eight = document.createElement("p");
-  createP_eight_eight.setAttribute("class", "resultText");
+  let createP_eight_eight = document.createElement('p');
+  createP_eight_eight.setAttribute('class', 'resultText');
   createP_eight_eight.innerText = `${userData.following}`;
 
   createDivColumnTwo.appendChild(createDivRowFour);
@@ -188,4 +199,13 @@ function reqFun(data) {
   createDivFlTwo.appendChild(createP_eight_eight);
 
   result.appendChild(createDivColumnTwo);
+}
+
+function reqError() {
+  document.getElementById('result').innerHTML = '';
+
+  let createErrorEl = document.createElement('div');
+  createErrorEl.setAttribute('class', 'errorDiv');
+  createErrorEl.innerText = 'No result';
+  result.appendChild(createErrorEl);
 }
